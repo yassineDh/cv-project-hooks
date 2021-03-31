8f16fb21990ebc46dbecf2d5b8b4a33a0d46dd23
DAHANE <ydahane@dxc.com>
2021-03-31 15:55:21 +0100

    complete general information

--- src/components/GeneralInformation.js
+++ src/components/GeneralInformation.js
@@ -1,33 +1,33 @@
 import React, { useState } from "react";
 
 function GeneralInformation() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState(0);
-  const [isComplete, setIsComplete] = useState(false);
+  const [generalInformation, setGeneralInformation] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: 0,
+    isComplete: false,
+  });
 
   function submitHandler(e) {
     e.preventDefault();
 
-    setIsComplete(true);
+    setGeneralInformation({...generalInformation,isComplete: true})
   }
 
   function updateHandler(e) {
     e.preventDefault();
 
-    setIsComplete(false);
+    setGeneralInformation({...generalInformation,isComplete: false})
   }
 
   function changeHandler(e) {
     const name = e.target.name;
     const value = e.target.value;
-    let methodName = "set" + name.charAt(0).toUpperCase() + name.slice(1);
-    console.log(methodName);
-    console.log(this);
-    methodName(value);
+    
+    setGeneralInformation({...generalInformation,[name]: value})
   }
-  let button = isComplete ? (
+  let button = generalInformation.isComplete ? (
     <button type="button" onClick={updateHandler}>
       Update
     </button>
@@ -45,28 +45,28 @@ function GeneralInformation() {
             name="firstName"
             placeholder="first name"
             onChange={changeHandler}
-            disabled={isComplete}
+            disabled={generalInformation.isComplete}
           />
           <input
             type="text"
             name="lastName"
             placeholder="last name"
             onChange={changeHandler}
-            disabled={isComplete}
+            disabled={generalInformation.isComplete}
           />
           <input
             type="email"
             name="email"
             placeholder="email"
             onChange={changeHandler}
-            disabled={isComplete}
+            disabled={generalInformation.isComplete}
           />
           <input
             type="number"
             name="phoneNumber"
             placeholder="phone number"
             onChange={changeHandler}
-            disabled={isComplete}
+            disabled={generalInformation.isComplete}
           />
         </fieldset>
         {button}
